0f65d972e30388ecff19ecae428878f6f781b2a9
Shelton-Chambule-Dev <Chambulesheltonsamuel@gmail.com>
2026-05-12 12:15:18 +0200

    ReservaRuim

--- ProjetoReserva/src/App/Program.java
+++ ProjetoReserva/src/App/Program.java
@@ -1,6 +1,44 @@
 package App;
-public class Program {
-    public static void main(String[] args) {
+import entities.Reserva;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+public class Program {
+    public static void main(String[] args) throws ParseException {
+
+          // Versao muito ruim
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+                // entrada de dados
+        System.out.println("Numero da sala: ");
+        int sala = sc.nextInt();
+        System.out.print("Data do dia da entrada (dd/MM/yyyy) : ");
+        Date diaEntrada = sdf.parse(sc.next());
+        System.out.print("Data do dia da entrada (dd/MM/yyyy) : ");
+        Date diaSaida = sdf.parse(sc.next());
+
+        if(!diaSaida.after(diaEntrada)){
+            System.out.println("Erro: a data de saida deve ser depois da data de entrada!");
+        }else{
+            Reserva reserva = new Reserva(sala,diaEntrada,diaSaida);
+            System.out.println("Reserva: "+reserva);
+            System.out.println();
+            System.out.println("Introduza a data da reserva  que deseja atualizar");
+            System.out.print("Data do dia da entrada (dd/MM/yyyy) : ");
+            diaEntrada = sdf.parse(sc.next());
+            System.out.print("Data do dia da entrada (dd/MM/yyyy) : ");
+            diaSaida = sdf.parse(sc.next());
+            Date now = new Date();
+            if(diaEntrada.before(now) || diaSaida.before(now)){
+                System.out.println("As datas deve estar no futuro nao no passado!");
+            }else if(!diaSaida.after(diaEntrada)){
+                System.out.println("Erro: a data de saida deve ser depois da data de entrada!");
+            }else {
+                reserva.atualizarDatas(diaEntrada,diaSaida);
+                System.out.println("Data atualizada: "+reserva);
+            }
+        }
     }
 }
--- ProjetoReserva/src/entities/Reserva.java
+++ ProjetoReserva/src/entities/Reserva.java
@@ -1,3 +1,45 @@
 package entities;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 public class Reserva {
+
+    private Integer nomeSala;
+    private Date diaEntrada;
+    private Date diaSaida;
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public Reserva(Integer nomeSala, Date diaEntrada, Date diaSaida) {
+        this.nomeSala = nomeSala;
+        this.diaEntrada = diaEntrada;
+        this.diaSaida = diaSaida;
+    }
+    public Integer getNomeSala() {
+        return nomeSala;
+    }
+    public void setNomeSala(Integer nomeSala) {
+        this.nomeSala = nomeSala;
+    }
+    public Date getDiaEntrada() {
+        return diaEntrada;
+    }
+    public Date getDiaSaida() {
+        return diaSaida;
+    }
+   // Removi so metodos set de diaEntrada e diaSaida
+    public  long Duracao(){
+        // para calcular a difeenca de dias entre diaEntrda e diaSaida usei os Milisegundos
+        // diferenca entra as duas datas diaEntrada e diaSaida em
+        long diferenca = diaEntrada.getTime() - diaSaida.getTime();
+        return  TimeUnit.DAYS.convert(diferenca,TimeUnit.MILLISECONDS);
+    }
+    public  void atualizarDatas( Date diaEntrada, Date diaSaida){
+           this.diaEntrada = diaEntrada;
+           this.diaSaida = diaSaida;
+    }
+    @Override
+    public String toString(){
+        return "Sala: "+nomeSala+", DiaEntrada : "+sdf.format(diaEntrada)+", DiaSaida:  "+sdf.format(diaSaida)+ " Duracao: " +Duracao()+ " Noites";
+    }
 }
